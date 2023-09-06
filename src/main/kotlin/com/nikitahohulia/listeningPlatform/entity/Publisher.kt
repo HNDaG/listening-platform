56882c2918b2b0470ea7cc60ad6c6b175a66cac0
HNDaG <nikitahohulia@gmail.com>
2023-09-06 14:25:29 +0300

    Adding Entities Setting up MySql container via Docker compose Setting tables auto-generation from entities

---
+++ src/main/kotlin/com/nikitahohulia/listeningPlatform/entity/Publisher.kt
@@ -0,0 +1,23 @@
+package com.nikitahohulia.listeningPlatform.entity
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.OneToMany
+import jakarta.persistence.Table
+
+@Entity
+@Table(name = "publisher")
+class Publisher(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Long? = null,
+    @Column(unique = true, nullable = false)
+    var publisherName: String,
+    @OneToMany
+    val posts: MutableSet<Post> = mutableSetOf(),
+    @Column(nullable = true)
+    var rating: Double? = null
+)
---
+++ src/main/kotlin/com/nikitahohulia/listeningPlatform/entity/User.kt
@@ -0,0 +1,30 @@
+package com.nikitahohulia.listeningPlatform.entity
+
+import jakarta.persistence.CascadeType
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.OneToMany
+import jakarta.persistence.OneToOne
+import jakarta.persistence.Table
+
+@Entity
+@Table(name = "users")
+class User(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null,
+    @Column(unique = true, nullable = false)
+    var username: String,
+    @Column(nullable = false)
+    var password: String,
+    @OneToMany
+    @JoinColumn(name = "subscription_id")
+    var subscriptions: MutableSet<Subscription> = mutableSetOf(),
+    @OneToOne(cascade = [CascadeType.ALL])
+    @JoinColumn(name = "publisher_id")
+    var publisher: Publisher? = null
+)
