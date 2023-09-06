c3164ec626607f5941db5922eb5366e1c0f1cf1c
HNDaG <nikitahohulia@gmail.com>
2023-09-06 15:29:49 +0300

    Adding detekt plugin

--- src/test/kotlin/com/nikitahohulia/listeningPlatform/ListeningPlatformApplicationTests.kt
+++ src/test/kotlin/com/nikitahohulia/listeningPlatform/ListeningPlatformApplicationTests.kt
@@ -6,8 +6,6 @@ import org.springframework.boot.test.context.SpringBootTest
 @SpringBootTest
 class ListeningPlatformApplicationTests {
 
-	@Test
-	fun contextLoads() {
-	}
-
+    @Test
+    fun contextLoads() = Unit
 }
