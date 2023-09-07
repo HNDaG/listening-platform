255f2addf345272b3a6e1d5145e57487fc90cbc2
HNDaG <nikitahohulia@gmail.com>
2023-09-07 10:32:02 +0300

    Init

---
+++ src/main/kotlin/com/nikitahohulia/listeningplatform/ListeningplatformApplication.kt
@@ -0,0 +1,11 @@
+package com.nikitahohulia.listeningplatform
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ListeningplatformApplication
+
+fun main(args: Array<String>) {
+	runApplication<ListeningplatformApplication>(*args)
+}
---
+++ src/test/kotlin/com/nikitahohulia/listeningplatform/ListeningplatformApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.nikitahohulia.listeningplatform
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ListeningplatformApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
