f1ecaf1748d117e19725d8d815bc756aa1755412
HNDaG <nikitahohulia@gmail.com>
2023-09-15 10:08:52 +0300

    Minor entities changes

--- src/main/kotlin/com/nikitahohulia/listeningplatform/entity/Publisher.kt
+++ src/main/kotlin/com/nikitahohulia/listeningplatform/entity/Publisher.kt
@@ -1,6 +1,5 @@
 package com.nikitahohulia.listeningplatform.entity
 
-import org.bson.codecs.pojo.annotations.BsonProperty
 import org.bson.types.ObjectId
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
@@ -9,7 +8,6 @@ import org.springframework.data.mongodb.core.mapping.Document
 data class Publisher(
     @Id
     val id: ObjectId? = null,
-    @BsonProperty(value = "publisher_name")
     val publisherName: String,
     val rating: Double? = null,
 )
