b6269a77be9cafe8704adc23ee4b626f23177e12
HNDaG <nikitahohulia@gmail.com>
2023-09-18 13:00:15 +0300

    Resolve conflict commit

--- src/main/kotlin/com/nikitahohulia/listeningplatform/repository/PostRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.nikitahohulia.listeningplatform.repository
-
-import com.nikitahohulia.listeningplatform.entity.Post
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface PostRepository : JpaRepository<Post, Long>
--- src/main/kotlin/com/nikitahohulia/listeningplatform/repository/UserRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.nikitahohulia.listeningplatform.repository
-
-import com.nikitahohulia.listeningplatform.entity.User
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface UserRepository : JpaRepository<User, Long>
