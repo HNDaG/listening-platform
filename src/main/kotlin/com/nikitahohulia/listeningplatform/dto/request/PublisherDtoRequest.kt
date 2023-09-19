cd6f41028f1a6d3b57a22a2a406e90c0f1c755e2
HNDaG <nikitahohulia@gmail.com>
2023-09-19 10:01:01 +0300

    Some entity change

--- src/main/kotlin/com/nikitahohulia/listeningplatform/dto/request/PublisherDtoRequest.kt
+++ src/main/kotlin/com/nikitahohulia/listeningplatform/dto/request/PublisherDtoRequest.kt
@@ -8,9 +8,12 @@ data class PublisherDtoRequest(
     val id: String?,
     @field:Size(min = 4, max = 16, message = "Publisher name must be at between 4 and 16 characters.")
     val publisherName: String,
+    @field:Size(min = 8, message = "Password is too short.")
+    val password: String
 )
 
 fun PublisherDtoRequest.toEntity() = Publisher(
     id = id?.let { ObjectId(it) },
-    publisherName = publisherName
+    publisherName = publisherName,
+    password = password
 )
