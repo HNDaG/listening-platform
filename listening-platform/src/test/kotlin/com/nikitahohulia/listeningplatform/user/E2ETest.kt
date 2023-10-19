1a393ba219914af7d4c31783c8e12932fbb2a4ea
HNDaG <nikitahohulia@gmail.com>
2023-10-19 11:28:21 +0300

    Refactoring project to reactive approach, nats + tests

---
+++ listening-platform/src/test/kotlin/com/nikitahohulia/listeningplatform/user/E2ETest.kt
@@ -0,0 +1,8 @@
+package com.nikitahohulia.listeningplatform.user
+
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+
+@ActiveProfiles("e2e")
+@SpringBootTest
+annotation class E2ETest
