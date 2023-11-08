79fa2b5a1cccd05db3924d1a85bdad32c6fc565e
HNDaG <nikitahohulia@gmail.com>
2023-11-08 12:22:35 +0200

    Fixing GitHub actions

--- listening-platform/src/test/kotlin/com/nikitahohulia/listeningplatform/user/E2ETest.kt
+++ listening-platform/src/test/kotlin/com/nikitahohulia/listeningplatform/user/E2ETest.kt
@@ -3,6 +3,6 @@ package com.nikitahohulia.listeningplatform.user
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
 
-@ActiveProfiles("e2e")
+@ActiveProfiles("docker")
 @SpringBootTest
 annotation class E2ETest
